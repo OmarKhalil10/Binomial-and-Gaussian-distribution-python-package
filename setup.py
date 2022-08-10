66f811d3f2ae25bc2e67a12481a23740a3887ad8
Omar Khalil <omarkhalil770@gmail.com>
2022-08-11 01:49:45 +0200

    uploading the package files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='omar_khalil_probability_package',
+      version='2.0',
+      description='Gaussian and Bionomial distributions ND',
+      packages=['omar_khalil_probability_package'],
+      author="Omar Khalil",
+      author_email="omar.khalil498@gmail.com",
+      zip_safe=False)
